c7489d08d0d7673503b968b9456f8ad0d0e5bf1b
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-06-09 21:39:42 -0500

    Initial Commit

---
+++ AI-Trip-API/AI_Trip_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Trip_APIApp.swift
+//  AI-Trip-API
+//
+//  Created by Renato Oliveira Fraga on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct AI_Trip_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
